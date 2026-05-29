fffedd39df93debc9af89efde2ea0a86d650fc7d
Roman Knyazev <i88ronin@icloud.com>
2026-05-29 19:30:59 +0700

    first release

--- bot.py
+++ bot.py
@@ -1,46 +1,72 @@
 import asyncio
 import aiohttp
-from aiogram import Bot, Dispatcher, html
-from aiogram.filters import CommandStart, Command
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-TOKEN = "YOUR_BOT_TOKEN"
+logging.basicConfig(level=logging.INFO)
+
+TOKEN = "YOUR_BOT_TOKEN`"
 dp = Dispatcher()
 
+BASE_URL = "https://v2.jokeapi.dev/joke/"
+VALID_CATEGORIES = ["Any", "Programming", "Spooky", "Dark"]
+START_KEYBOARD = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Any"), KeyboardButton(text="Programming")],
+        [KeyboardButton(text="Spooky"), KeyboardButton(text="Dark")]
+    ],
+    resize_keyboard=True
+)
+
+# Handler start
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello!Send me a Pokemon name you want to see!")
+    await message.answer(f"Hello! I know different jokes! Do you want to know one of them?",
+    reply_markup = START_KEYBOARD
+    )
 
+# Sent Joke if user type any message
 @dp.message()
-async def send_pokemon_info(message: Message):
-    pokemon_name = message.text.lower().strip()  # type: ignore
-    await message.answer("I'm looking for your Pokemon!")
+async def send_joke(message: Message, session: aiohttp.ClientSession):
+    #joke = message.text.lower().strip()  # type: ignore
+    category = message.text
 
-    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+    if category not in VALID_CATEGORIES:
+        await message.answer("Use buttons to choose valid category!")
+        return
+    
+    await message.answer("I'm thinking of joke...")
+    await asyncio.sleep(4)
+    await message.answer("Oh, I got it! Listen!")
+    await asyncio.sleep(1)
 
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(url) as response:
-                if response.status == 200:
-                    data = await response.json()
-                    name = data["name"].capitalize()
-                    image_url = data["sprites"]["front_default"]
-                    pokemon_type = data["types"][0]["type"]["name"]
-                    caption_text = f"👾 Pokemon: {name}\n⚡ Тип: {pokemon_type}"
-                    if image_url:
-                        await message.answer_photo(photo = image_url, caption = caption_text)
-                    else:
-                        await message.answer(caption_text)
-                elif response.status == 404:
-                    await message.answer("Enter a name in English! Don't use numbers and symbols!")
+    url = f"{BASE_URL}{category}"
+
+    try:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+                if data["type"] == "single":
+                    await message.answer(data["joke"])
                 else:
-                    await message.answer(f"Error! Error's code is {response.status}")
-        except Exception as e:
+                    await message.answer(data["setup"])
+                    await asyncio.sleep(2)
+                    await message.answer(data["delivery"])
+            else:
+                await message.answer(f"Error! Code: {response.status}")
+    except Exception as e:
+            logging.error(f"Error! Error type is {e}")
             await message.answer("Something went wrong! Try again later!")
 
 async def main():
-    bot = Bot(token=TOKEN)
-    await dp.start_polling(bot)
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    async with aiohttp.ClientSession() as session:
+        await dp.start_polling(bot, session=session)
 
 if __name__ == "__main__":
     asyncio.run(main())
